209b6c751050db3902980d1d176e0d9cf0b63c8d
Corey Steiner <corey.joseph.steiner@gmail.com>
2025-04-04 16:33:17 -0400

    updates to use .env file

--- server.ts
+++ server.ts
@@ -1,1 +1,14 @@
-console.log("Hello world!")
+import express from 'express'
+import http from 'http'
+
+const app = express()
+const server = http.createServer(app);
+const port = process.env.PORT || 'PORT NOT SET'
+
+app.get('/', (req, res) => {
+    res.sendfile('Hello World!')
+})
+
+server.listen(port, () => {
+    console.log(`Server listening on PORT:${port}`)
+})
