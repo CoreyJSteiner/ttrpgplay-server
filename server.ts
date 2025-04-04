dc4d562e47e1d1d8876dd2f7492a61dc0d2bb43c
Corey Steiner <corey.joseph.steiner@gmail.com>
2025-04-04 17:01:03 -0400

    updates server to send chat html

--- server.ts
+++ server.ts
@@ -1,12 +1,17 @@
 import express from 'express'
 import http from 'http'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 const app = express()
+app.use(express.static("public"));
 const server = http.createServer(app);
 const port = process.env.PORT || 'PORT NOT SET'
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.get('/', (req, res) => {
-    res.sendfile('Hello World!')
+    res.sendFile(__dirname + 'public/index.html')
 })
 
 server.listen(port, () => {
