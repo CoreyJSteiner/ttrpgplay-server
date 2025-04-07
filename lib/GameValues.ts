27017a62aff3156f913356acc7b8fa5536326cd1
Corey Steiner <corey.joseph.steiner@gmail.com>
2025-04-07 10:40:25 -0400

    adds basic GameValue class

---
+++ lib/GameValues.ts
@@ -0,0 +1,58 @@
+type EffectProperty = 'Temp' | 'Default'
+
+
+interface Effect {
+    value: number,
+    prop: EffectProperty
+}
+
+type Effects = Array<Effect>
+
+type Sheet = Record<string, GameValue>
+
+class GameValue {
+    name: string
+    baseValue: number
+    owner: string
+    effects: Effects
+
+    constructor(baseValue: number, name?: string, owner?: string, effects?: Effects) {
+        this.name = name || ""
+        this.owner = owner || ""
+        this.baseValue = baseValue
+        this.effects = effects || []
+    }
+
+    invoke(): number {
+        let effectMod = 0
+
+        for (let i = 0; i < this.effects.length; i++) {
+            const effect = this.effects[i]
+            if (effect.prop === 'Temp') {
+                // await prompt owner(s) input
+                // if not in effect remove and continue to next iteration
+            }
+
+            effectMod += effect.value
+        }
+
+        return this.baseValue + effectMod
+    }
+
+    setValue(value: number, owner: string): number {
+        if (this.isOwner(owner)) {
+            this.baseValue = value
+        } else {
+            throw new Error("Only owners may set value");
+        }
+
+        return value
+    }
+
+    isOwner(owner: string): boolean {
+        if (owner === this.owner) return true
+        return false
+    }
+}
+export type { Sheet }
+export { GameValue }
--- server.ts
+++ server.ts
@@ -5,6 +5,8 @@ import { fileURLToPath } from 'url'
 import { Server, Socket } from 'socket.io'
 import crypto from 'crypto'
 import { DiceRoll } from '@dice-roller/rpg-dice-roller'
+import { GameValue } from './lib/GameValues.ts'
+import type { Sheet } from './lib/GameValues.ts'
 
 const app = express()
 app.use(express.static("public"));
@@ -41,22 +43,31 @@ function createUUID(): UUID {
     return crypto.randomUUID() as UUID
 }
 
-type CharSheet = Record<string, number>
+const testCharSheet: Sheet = {
+    "HP": new GameValue(666),
+    "AC": new GameValue(20),
+    "STR": new GameValue(10),
+    "DEX": new GameValue(11),
+    "CON": new GameValue(12),
+    "INT": new GameValue(13),
+    "WIS": new GameValue(14),
+    "CHA": new GameValue(15)
+}
 
-const testCharSheet: CharSheet = {
-    HP: 666,
-    AC: 20,
-    STR: 10,
-    DEX: 9,
-    CON: 11,
-    INT: 13,
-    WIS: 13,
-    CHA: 15
+const testCharSheet1: object = {
+    "HP": 66,
+    "AC": 20,
+    "STR": 10,
+    "DEX": 11,
+    "CON": 12,
+    "INT": 13,
+    "WIS": 14,
+    "CHA": 15
 }
 
 function handleMessage(msg: string, userID): void {
     if (msg.slice(0, 2) === "/c") {
-        io.emit('character sheet display', testCharSheet)
+        io.emit('character sheet display', testCharSheet1)
         return
     }
 
@@ -69,7 +80,7 @@ function parseMessage(msg: string): DiceRoll | string {
     console.log(msg.slice(0, 2))
     if (msg.slice(0, 3) === "/r ") {
         try {
-            const subMsg = subTokens(msg, testCharSheet)
+            const subMsg = subTokens(msg, testCharSheet1)
             const roll = new DiceRoll(subMsg.slice(3))
             return roll
         } catch (error) {
@@ -80,7 +91,7 @@ function parseMessage(msg: string): DiceRoll | string {
     return msg
 }
 
-function subTokens(input: string, charSheet: CharSheet): string {
+function subTokens(input: string, charSheet: object): string {
     // Regex to find all # followed by word characters (like #DEX)
     const tokenRegex = /#(\w+)/g;
 
