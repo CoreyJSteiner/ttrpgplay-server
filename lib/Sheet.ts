9b1946738e678297afd916a8290c5c57a6458cf3
Corey Steiner <corey.joseph.steiner@gmail.com>
2025-04-19 19:43:16 -0400

    ideation SnapSheet

--- lib/Sheet.ts
+++ lib/Sheet.ts
@@ -38,4 +38,22 @@ class GVMSheet {
 
 }
 
+// This sort of sheet is for creating values that override the GVM values. If a value is not present it will default to 
+// the GVM sheet which will pull values based on id (name?). The question that arrises is should you then be able to 
+// swap the breakout and GVM sheet? And why bother? Should the GVM sheet just have the breajkout values natively? I 
+// think for the sake of organization, I'd like to separate them, but realistically all sheets will be breakout sheets.
+// This will help segemnt the logic a bit and keep it clean. GVM should be able to target value Params like owner and
+// name so that I can say 'corey@dex'. But i don;t know now that seems messy. why not just let the sheet hold the value?
+// Beacuse I want to be able to say 'player@dex'. Ok so then maybe the paradigm is inverted and breakouts are used for
+// archived sheets? Maybe. Yeah, the system should favor using GVM but should be abnle to use breakouts for exceptions,
+// pulling an old character sheet seems like a good usecase. 'Saved Sheet' type thing. Snapshot.
+class SnapSheet {
+    overrideValues: object
+
+    constructor(overrideValues: object) {
+        this.overrideValues = overrideValues
+    }
+
+}
+
 export { GVMSheet }
